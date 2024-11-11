38107793ac2e1f83cc16ac8e9331ce8a9f7c3f82
Carlos Henrique <110176211+chenriquedev@users.noreply.github.com>
2024-11-11 16:01:04 -0300

    Added proxy (#5)

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,18 +1,17 @@
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
 
-  if (import.meta.server) {
-    const api = $fetch.create({
-      baseURL: 'https://api.themoviedb.org/3',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${config.apiSecret}`,
-      },
-    });
-    return {
-      provide: {
-        api,
-      },
-    };
-  }
+  const api = $fetch.create({
+    baseURL: '/api',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${config.public.apiSecret}`,
+    },
+  });
+  return {
+    provide: {
+      api,
+    },
+  };
 });
+  
