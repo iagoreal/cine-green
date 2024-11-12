87c609a0873303c84ab06ccd1b66b4622b7c5275
Carlos Henrique <110176211+chenriquedev@users.noreply.github.com>
2024-11-12 16:53:10 -0300

    fix fetch (#8)

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div v-if="status !== 'success'">Carregando</div>
   <div
-    v-else
     v-for="movieOrTvShow in moviesAndTvShows"
     class="group col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 transform transition duration-300 ease-in-out hover:bg-gray-800 hover:scale-95 rounded-lg"
   >
@@ -15,16 +13,15 @@
 import { TrendingAll } from '~/model/TrendingAll/TrendingAll.model';
 
 const { $api } = useNuxtApp();
-let moviesAndTvShows = ref<TrendingAll[]>();
+let moviesAndTvShows: Ref<TrendingAll[]> = ref([]);
 
 const useGetTrendingAll = getTrendingAll($api);
-const { data, status } = await useAsyncData(() => useGetTrendingAll.get(), {
-  server: false,
-});
 
-watchEffect(() => {
-  if (data.value) {
-    moviesAndTvShows.value = data.value.results || [];
-  }
-});
+async function getAllMoviesAndTvShow() {
+  useGetTrendingAll.get().then((res) => {
+    moviesAndTvShows.value = [...res.results!];
+  });
+}
+
+onMounted(() => getAllMoviesAndTvShow());
 </script>
--- pages/tvshow/index.vue
+++ pages/tvshow/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div v-if="status !== 'success'">Carregando</div>
   <div
-    v-else
     v-for="tvShow in tvShows"
     class="group col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 transform transition duration-300 ease-in-out hover:bg-gray-800 hover:scale-95 rounded-lg"
   >
@@ -15,16 +13,15 @@
 import type { TvShowModel } from '~/model/tvShow/tvShow.model';
 
 const { $api } = useNuxtApp();
-let tvShows = ref<TvShowModel[]>();
+let tvShows: Ref<TvShowModel[]> = ref([]);
 
-const useGetTrendingAll = getAllTvShow($api);
-const { data, status } = await useAsyncData(() => useGetTrendingAll.get(), {
-  server: false,
-});
+const useGetTvShow = getTvShow($api);
 
-watchEffect(() => {
-  if (data.value) {
-    tvShows.value = data.value.results || [];
-  }
-});
+async function getAllTvShow() {
+  useGetTvShow.get().then((res) => {
+    tvShows.value = [...res?.results!];
+  });
+}
+
+onMounted(() => getAllTvShow());
 </script>
