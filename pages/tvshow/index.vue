b17697e49eb5f2e910fe4aca3f7262a6e18ca886
chenriquedev <krlos.henrique98@gmail.com>
2024-11-12 14:52:29 -0300

    Fix request

--- pages/tvshow/index.vue
+++ pages/tvshow/index.vue
@@ -2,19 +2,29 @@
   <div v-if="status !== 'success'">Carregando</div>
   <div
     v-else
-    v-for="movieOrTvShow in moviesAndTvShows"
+    v-for="tvShow in tvShows"
     class="group col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 transform transition duration-300 ease-in-out hover:bg-gray-800 hover:scale-95 rounded-lg"
   >
     <CardComponent
-      :imageEndpoint="movieOrTvShow?.poster_path"
-      :type="movieOrTvShow?.media_type"
+      :imageEndpoint="tvShow?.poster_path"
+      :type="tvShow?.media_type"
     />
   </div>
 </template>
 <script setup lang="ts">
+import type { TvShowModel } from '~/model/tvShow/tvShow.model';
+
 const { $api } = useNuxtApp();
+let tvShows = ref<TvShowModel[]>();
 
 const useGetTrendingAll = getAllTvShow($api);
-const { data, status } = await useAsyncData(() => useGetTrendingAll.get());
-const moviesAndTvShows = data.value?.results;
+const { data, status } = await useAsyncData(() => useGetTrendingAll.get(), {
+  server: false,
+});
+
+watchEffect(() => {
+  if (data.value) {
+    tvShows.value = data.value.results || [];
+  }
+});
 </script>
--- utils/movie.rest.ts
+++ utils/movie.rest.ts
@@ -4,7 +4,6 @@ import type { Movie } from '~/model/Movie/Movie.model';
 
 export const getMovies = <T>(fetch: $Fetch<T, NitroFetchRequest>) => ({
   async get(params?: {}): Promise<FetchResponse<Movie>> {
-    return fetch<FetchResponse<Movie>>('/movie/now_playing', params,
-    );
+    return fetch<FetchResponse<Movie>>('/movie/now_playing', params);
   },
 });
