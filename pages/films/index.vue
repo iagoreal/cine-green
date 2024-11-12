786a43b5b37a34d5266c2c64d4b16190d156894d
chenriquedev <krlos.henrique98@gmail.com>
2024-11-12 16:43:31 -0300

    fix fetch

--- pages/films/index.vue
+++ pages/films/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div v-if="status !== 'success'">Carregando</div>
   <div
-    v-else
     v-for="movie in movies"
     class="group col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 transform transition duration-300 ease-in-out hover:bg-gray-800 hover:scale-95 rounded-lg"
   >
@@ -15,16 +13,15 @@
 import type { Movie } from '~/model/Movie/Movie.model';
 
 const { $api } = useNuxtApp();
-let movies = ref<Movie[]>();
+let movies: Ref<Movie[]> = ref([]);
 
 const useGetMovies = getMovies($api);
-const { data, status } = await useAsyncData(() => useGetMovies.get(), {
-  server: false,
-});
 
-watchEffect(() => {
-  if (data.value) {
-    movies.value = data.value.results || [];
-  }
-});
+async function getAllMovies() {
+  useGetMovies.get().then((res) => {
+    movies.value = [...res?.results!];
+  });
+}
+
+onMounted(() => getAllMovies());
 </script>
--- utils/tvShow.rest.ts
+++ utils/tvShow.rest.ts
@@ -1,7 +1,7 @@
 import type { NitroFetchRequest, $Fetch } from 'nitropack';
 import type FetchResponse from '~/model/FetchResponse/FetchResponse.model';
 import type { TvShowModel } from '~/model/tvShow/tvShow.model';
-export const getAllTvShow = <T>(fetch: $Fetch<T, NitroFetchRequest>) => ({
+export const getTvShow = <T>(fetch: $Fetch<T, NitroFetchRequest>) => ({
   async get(params?: {}): Promise<FetchResponse<TvShowModel>> {
     return fetch<FetchResponse<TvShowModel>>(
       '/trending/tv/day?language=en-US',
